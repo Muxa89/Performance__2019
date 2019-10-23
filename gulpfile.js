a8ad9f29cc3fe2e76b41919c17a77191973d6e32
Muxa89 <muxa.89@inbox.ru>
2019-10-23 18:39:16 +0400

    Minified css.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 const gulp = require("gulp");
 const minify = require("gulp-minify");
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task("compress", function() {
   gulp
     .src("js_orig/*.js")
     .pipe(minify())
     .pipe(gulp.dest("js"));
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('css/min'));
 });
