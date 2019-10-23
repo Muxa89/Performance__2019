76397a68d0a64cedd7357f06268afb60ea020b50
Muxa89 <muxa.89@inbox.ru>
2019-10-23 22:25:57 +0400

    Purged unused css.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const minify = require("gulp-minify");
-const cleanCSS = require('gulp-clean-css');
+const cleanCSS = require("gulp-clean-css");
+const purgecss = require("gulp-purgecss");
 
 gulp.task("compress", function() {
   gulp
@@ -9,8 +10,20 @@ gulp.task("compress", function() {
     .pipe(gulp.dest("js"));
 });
 
-gulp.task('minify-css', () => {
-  return gulp.src('css/orig/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('css'));
+gulp.task("minify-css", () => {
+  return gulp
+    .src("css/purged/*.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("css"));
+});
+
+gulp.task("purgecss", () => {
+  return gulp
+    .src("css/orig/*.css")
+    .pipe(
+      purgecss({
+        content: ["index.html"]
+      })
+    )
+    .pipe(gulp.dest("css/purged"));
 });
